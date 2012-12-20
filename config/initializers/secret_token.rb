bbb9c110042465151b5a9938ad6ae15eca7e635a
cookr <ciaran@cookr.com>
2012-12-20 16:22:18 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fionn3::Application.config.secret_token = '633c96b57cb041835da905fe343573d2be22ef0639d71fe240b073ea646135e2f83cae7a0f6da9dd743d47a4f29f556663196d26fb6e138e703751cefc586125'
